9a54474f5ffc6c1b7cc32641a987381e6400e42e
Vladislav Kabakov <zloy.gololed@gmail.com>
2023-07-01 16:49:10 +0400

    feat: tailwindcss

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="bg-red-500 text-3xl font-thin underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'tailwindcss'
+// import defaultTheme from 'tailwindcss/defaultTheme'
+// import colors from 'tailwindcss/colors'
+
+export default <Partial<Config>>{
+    theme: {
+        extend: {
+
+        }
+    }
+}
