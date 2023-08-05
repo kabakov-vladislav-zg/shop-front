0461f66053b7887c0813ab7800b4e3095f9967d6
Vladislav Kabakov <zloy.gololed@gmail.com>
2023-08-06 00:38:46 +0400

    feat(UICarousel): useDots TSDoc

--- components/app/VCarousel/dots.ts
+++ components/app/VCarousel/dots.ts
@@ -3,14 +3,17 @@ import { Breakpoint, breakpoints } from '~/utils/breakpoints';
 
 type CapacityOptions = string | number | undefined | Partial<Record<Breakpoint, string | number | undefined>>;
 
-const getPagesOptionsEntries = (capacityOptions: CapacityOptions, itemsCount: number) => {
-  const pagesOptionsEntries : [Breakpoint, number][] = [];
+ /**
+   * Returns matrix of visible dots quantity per breakpoint.
+   */
+const getPagesOptionsMatrix = (capacityOptions: CapacityOptions, itemsCount: number) => {
+  const pagesOptionsMatrix : [Breakpoint, number][] = [];
   if (typeof capacityOptions === 'object') {
     const optionsEntries = Object.entries(capacityOptions);
     optionsEntries.forEach(([breakpoint, capacity]) => {
       const option = Number(capacity);
       if (!option) return;
-      pagesOptionsEntries.push([
+      pagesOptionsMatrix.push([
         breakpoint as Breakpoint,
         Math.ceil(itemsCount / option),
       ]);
@@ -18,47 +21,65 @@ const getPagesOptionsEntries = (capacityOptions: CapacityOptions, itemsCount: nu
   } else {
     const option = Number(capacityOptions);
     if (option) {
-      pagesOptionsEntries.push([
+      pagesOptionsMatrix.push([
         'xs',
         Math.ceil(itemsCount / option),
       ]);
     }
   }
-  return pagesOptionsEntries;
+  return pagesOptionsMatrix;
 };
-const getDots = (max: number, options: Partial<Record<Breakpoint, number>>) => {
-  const states: boolean[] = new Array(max).fill(true);
+
+/**
+  * Returns array of dots classes.
+  */
+const getDots = (optionsMatrix: [Breakpoint, number][]) => {
+  // Calculate maximum possible number of dots.
+  const max = Math.max(...optionsMatrix.map(([, value]) => value));
+  const options = Object.fromEntries(optionsMatrix);
+  // Array of dots css classes.
   const result: string[][] = new Array(max).fill(1).map(() => ['VCarousel-Controls']);
-  const points = breakpoints.keys.filter((point) => point in options);
-  points.forEach((point) => {
-    const pagesCount = options[point] as number;
+  // Actual visibility statuses for each dot.
+  const states: boolean[] = new Array(max).fill(true);
+  // Filter out breakpoints with no option.
+  const actualBreakpoints = breakpoints.keys.filter((point) => point in options);
+  actualBreakpoints.forEach((breakpoint) => {
+    // For each breakpoint calculate quantity of visible dots.
+    const dotsQuantity = options[breakpoint] as number;
     result.forEach((page, index) => {
-      const state = index < pagesCount;
-      if (states[index] === state) return;
-      const className = state ? 'isVisible' : 'isHidden';
-      const classPoint = point === 'xs' ? '' : `-${point}`;
+      // For each point calculate its visibility status.
+      const isVisible = index < dotsQuantity;
+      // If visibility status for this dot has not changed - go to the next dot.
+      if (states[index] === isVisible) return;
+      // Else - update visibility status and set class.
+      states[index] = isVisible;
+      const className = isVisible ? 'isVisible' : 'isHidden';
+      const classPoint = breakpoint === 'xs' ? '' : `-${breakpoint}`;
       page.push(className + classPoint);
-      states[index] = state;
     });
   });
   return result;
 };
 
+ /**
+   * Reactive array of dots classes.
+   * @param isShowDots - Whether to display the dots panel.
+   * @param capacityOptions - Capacity per breakpoint.
+   * @param itemsCount - Carousel items quantity.
+   */
 export function useDots(
   isShowDots: MaybeRefOrGetter<boolean>,
   capacityOptions: MaybeRefOrGetter<CapacityOptions>,
   itemsCount : MaybeRefOrGetter<number>
 ) {
   const dots = computed(() => {
+    // If there is no dots panel, do no calculations.
     if (!toValue(isShowDots)) return [];
-    const pagesOptionsEntries = getPagesOptionsEntries(
+    const pagesOptionsMatrix = getPagesOptionsMatrix(
       toValue(capacityOptions),
       toValue(itemsCount),
     );
-    const pages = pagesOptionsEntries.map(([, value]) => value);
-    const max = Math.max(...pages);
-    const options = Object.fromEntries(pagesOptionsEntries);
-    return getDots(max, options);
+    return getDots(pagesOptionsMatrix);
   });
   return { dots };
 }
